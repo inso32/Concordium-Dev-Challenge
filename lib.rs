c35ee756bbd8bb53e476a468a24e7922392f900f
Fomalhaut <inso32@gmail.com>
2023-11-21 09:06:00 +0500

    Update lib.rs

--- lib.rs
+++ lib.rs
@@ -1,3 +1,6 @@
 /*!
  * Contract that requires 3 different accounts signatures to authorize some CCD transfers.
  */
+
+#![cfg_attr(not(feature = "std"), no_std)]
+use concordium_std::{collections::*, *};
